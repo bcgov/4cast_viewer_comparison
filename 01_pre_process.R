eebcfc53a1c62e14fd4f3e5dcdbe6364abdd284b
Rich-P-Martin <richard.martin@gov.bc.ca>
2024-04-22 10:35:45 -0700

    added occupation shares

--- 01_pre_process.R
+++ 01_pre_process.R
@@ -122,6 +122,59 @@ lfs_data <- full_join(lfs_data, lfs_data_totals)
 
 write_csv(lfs_data, here("out","lfs_data.csv"))
 
+#occupation shares-------------------
+
+stokes_emp_occ <- read_excel(here("data",
+                                  "occupation_new",
+                                  "employment_occupation.xlsx"), skip=3)|>
+  filter(`Geographic Area`=="British Columbia",
+         NOC!="#T")|>
+  mutate(Description=if_else(str_detect(Description, "Seniors"),
+                             "Senior managers - public and private sector",
+                             Description))|>
+  select(Description, starts_with("2"))|>
+  pivot_longer(cols=starts_with("2"), names_to = "year", values_to = "count")|>
+  mutate(year=as.numeric(year),
+         sample="stokes")|>
+  filter(year >= year(today()))|>
+  group_by(year)|>
+  mutate(share=count/sum(count))|>
+  select(-count)|>
+  group_by(Description, sample, .add = FALSE)|>
+  mutate(weight = .5^(year-max(year)+1)/sum(.5^(year-max(year)+1)))
+
+
+
+lfs_emp_occ <- read_excel(here("data",
+                               "Labour force status for 5 digit NOC 2014-2023.xlsx"),
+                          skip=3,
+                          sheet = "Employed")|>
+  filter(`Noc 5`!="Total")|>
+  select(-`Noc 5`)|>
+  rename(Description=`Class Title`)|>
+  pivot_longer(cols=starts_with("2"), names_to = "year", values_to = "count")|>
+  mutate(Description=if_else(str_detect(Description, "Senior managers|Senior government managers"),
+                             "Senior managers - public and private sector",
+                             Description))|>
+  group_by(Description, year)|>
+  summarize(count=sum(count, na.rm = TRUE))|>
+  mutate(year=as.numeric(year))|>
+  group_by(year)|>
+  mutate(share=count/sum(count, na.rm=TRUE),
+         sample="lfs")|>
+  select(-count)|>
+  group_by(Description, sample, .add = FALSE)|>
+  mutate(weight = .5^(max(year)-year+1)/sum(.5^(max(year)-year+1)))
+
+
+occ_shares <- full_join(stokes_emp_occ, lfs_emp_occ)
+
+occ_shares_weighted <- occ_shares|>
+  summarize(weighted_mean=sum(weight*share))|>
+  pivot_wider(names_from = sample, values_from = weighted_mean)
+
+write_csv(occ_shares, here("out","occ_shares.csv"))
+write_csv(occ_shares_weighted, here("out","occ_shares_weighted.csv"))
 
 
 
